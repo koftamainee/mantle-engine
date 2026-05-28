87fef5b6d9af7953f6f69567e501ede01041720d
kofta <koftamainee@gmail.com>
2026-05-28 12:47:27 +0300

    add noise sampler

---
+++ mantle/src/noise/private/sampler.cpp
@@ -0,0 +1,18 @@
+#include "noise/sampler.h"
+
+namespace mantle::detail {
+    glm::vec2 seed_offset(u32 seed) {
+        return glm::vec2{
+            static_cast<f32>(seed) * 31.7f,
+            static_cast<f32>(seed) * 73.1f,
+        };
+    }
+
+    glm::vec3 seed_offset_3d(u32 seed) {
+        return glm::vec3{
+            static_cast<f32>(seed) * 31.7f,
+            static_cast<f32>(seed) * 73.1f,
+            static_cast<f32>(seed) * 97.3f,
+        };
+    }
+} // namespace mantle::detail
